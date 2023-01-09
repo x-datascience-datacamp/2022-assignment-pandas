80d055c15436fc8980fad92d4d2749ae2e2372f0
Windu12345 <32morvantheo@gmail.com>
2023-01-09 16:25:27 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -11,13 +11,14 @@ aggregate them by regions and finally plot them on a map using `geopandas`.
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import os
 
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.DataFrame({})
-    regions = pd.DataFrame({})
-    departments = pd.DataFrame({})
+    referendum = pd.read_csv(os.path.join("data", "referendum.csv"), sep=";")
+    regions = pd.read_csv(os.path.join("data", "regions.csv"))
+    departments = pd.read_csv(os.path.join("data", "departments.csv"))
 
     return referendum, regions, departments
 
@@ -28,8 +29,16 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
-    return pd.DataFrame({})
+    regions = regions.rename(
+        columns={"code": "code_reg", "id": "id_reg", "name": "name_reg"}
+    )
+    departments = departments.rename(
+        columns={"code": "code_dep", "region_code": "code_reg", "name": "name_dep"}
+    )
+    df_merge = regions.merge(
+        departments, left_on="code_reg", right_on="code_reg", how="inner"
+    )
+    return df_merge[["code_reg", "name_reg", "code_dep", "name_dep"]]
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -38,8 +47,16 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
-    return pd.DataFrame({})
+    referendum["Department code"] = referendum["Department code"].apply(
+        lambda element: element.zfill(2)
+    )
+    df_merge = referendum.merge(
+        regions_and_departments,
+        left_on="Department code",
+        right_on="code_dep",
+        how="inner",
+    )
+    return df_merge
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -48,8 +65,11 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
-    return pd.DataFrame({})
+    df_group = referendum_and_areas.groupby("name_reg")[
+        ["Registered", "Abstentions", "Null", "Choice A", "Choice B"]
+    ].sum()
+    df_group = df_group.reset_index()
+    return df_group
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -61,22 +81,29 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
+    df_geo_regions = gpd.read_file(os.path.join("data", "regions.geojson")).rename(
+        columns={"code": "code_reg", "nom": "name_reg"}
+    )
+    df_final = referendum_result_by_regions.merge(
+        df_geo_regions[["name_reg", "geometry"]], on="name_reg", how="inner"
+    )
 
-    return gpd.GeoDataFrame({})
+    df_final = gpd.GeoDataFrame(df_final)
+    df_final["ratio"] = df_final["Choice A"] / (
+        df_final["Choice A"] + df_final["Choice B"]
+    )  # +df_final['Null'])
+    df_final.plot("ratio")
+    return df_final  # [['ratio','geometry']]
 
 
 if __name__ == "__main__":
 
     referendum, df_reg, df_dep = load_data()
-    regions_and_departments = merge_regions_and_departments(
-        df_reg, df_dep
-    )
+    regions_and_departments = merge_regions_and_departments(df_reg, df_dep)
     referendum_and_areas = merge_referendum_and_areas(
         referendum, regions_and_departments
     )
-    referendum_results = compute_referendum_result_by_regions(
-        referendum_and_areas
-    )
+    referendum_results = compute_referendum_result_by_regions(referendum_and_areas)
     print(referendum_results)
 
     plot_referendum_map(referendum_results)
