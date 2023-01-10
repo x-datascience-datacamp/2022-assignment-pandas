554487a82c8f7d37292854d4479d15c63d0a5bb6
Spaectus <30037190+Spaectus@users.noreply.github.com>
2023-01-10 13:53:55 +0100

    Update pandas_questions.py

--- pandas_questions.py
+++ pandas_questions.py
@@ -17,7 +17,6 @@ import matplotlib.pyplot as plt
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-
     referendum = pd.read_csv("data/referendum.csv", sep=";")
     regions = pd.read_csv("data/regions.csv", sep=",")
     departments = pd.read_csv("data/departments.csv", sep=",")
@@ -31,7 +30,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     return pd.merge(
         regions,
         departments,
@@ -50,7 +48,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     referendum["Department code"] = referendum["Department code"].apply(
         lambda x: x.zfill(2)
     )
@@ -68,7 +65,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     return (
         referendum_and_areas.groupby(["name_reg", "code_reg"], as_index=False)
         .agg(
@@ -93,7 +89,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     geo_reg = gpd.read_file("data/regions.geojson")
     geo_reg = geo_reg.merge(
         referendum_result_by_regions, left_on="code", right_index=True
