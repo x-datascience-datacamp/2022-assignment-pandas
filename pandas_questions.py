f7d6650a911711d962a8b1bda8202075e5ab2e9b
Dave Dahan <davedahan@MacBook-Pro-de-Dave.local>
2023-01-10 17:52:10 +0100

    Pandas sol

--- pandas_questions.py
+++ pandas_questions.py
@@ -86,7 +86,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     df_reg = gpd.read_file('data/regions.geojson')
     df_reg = df_reg.drop(index=range(9, 14))
     df = referendum_result_by_regions
