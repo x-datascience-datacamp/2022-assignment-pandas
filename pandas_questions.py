9699457285fc93bab1e84100fec4e6c2a7f7478a
Ahmad-Khalifeh-11 <khalifeh-ahmad@hotmail.com>
2023-01-10 10:54:22 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -24,10 +24,10 @@ def load_data():
 
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
+
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     regions = regions.rename(columns={'code': 'region_code'})
     regions_and_departments = pd.merge(
         regions[["region_code", "name"]],
@@ -45,10 +45,10 @@ def merge_regions_and_departments(regions, departments):
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
     """Merge referendum and regions_and_departments in one DataFrame.
+
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     referendum_and_areas = referendum.drop(
         referendum[referendum["Department code"].str.startswith("Z")].index
         )
@@ -66,10 +66,10 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
 
 def compute_referendum_result_by_regions(referendum_and_areas):
     """Return a table with the absolute count for each region.
+
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     Result = referendum_and_areas.loc[:, [
         'code_reg',
         'name_reg',
