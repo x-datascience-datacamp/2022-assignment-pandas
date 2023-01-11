ea82adeee6875d15122495bedec89af549c1c540
Romain BERNARD <romain.bernard@telecom-sudparis.eu>
2023-01-11 15:50:04 +0100

    Fix format

--- pandas_questions.py
+++ pandas_questions.py
@@ -28,7 +28,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     merge_reg_dep = pd.merge(
         regions,
         departments,
@@ -46,7 +45,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     referendum["Department code"] = referendum["Department code"].str.zfill(2)
     merge_ref_areas = pd.merge(
         referendum,
@@ -64,7 +62,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     groupbyregions = referendum_and_areas.groupby(
         ["code_reg", "name_reg"]
     ).sum()
@@ -83,7 +80,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     geo_pos = gpd.read_file("./data/regions.geojson")
     merge_df = pd.merge(
         geo_pos,
