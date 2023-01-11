4d4064ceee277dd0d4d63e27c3d3a0f3583be087
Abed <abedshaar1999@gmail.com>
2023-01-11 03:33:07 +0100

    fixing pydocstyle format

--- pandas_questions.py
+++ pandas_questions.py
@@ -18,7 +18,6 @@ import matplotlib.pyplot as plt
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-
     referendum = pd.read_csv('./data/referendum.csv', sep=';')
     regions = pd.read_csv('./data/regions.csv', sep=',')
     departments = pd.read_csv('./data/departments.csv', sep=',')
@@ -29,11 +28,9 @@ def load_data():
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
 
-
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     df = regions.merge(departments, left_on='code',
                        right_on='region_code', suffixes=('_reg', '_dep')
                        )
@@ -49,7 +46,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     df = referendum.drop(referendum[referendum['Department code'].
                                     str.startswith('Z')].index)
     df['Department code'] = df['Department code'].str.zfill(2)
@@ -65,7 +61,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     df = referendum_and_areas.loc[:, [
         'code_reg',
         'name_reg',
@@ -89,7 +84,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     geo_data = gpd.read_file('data/regions.geojson')
     df = pd.merge(geo_data, referendum_result_by_regions, left_on='code',
                   right_index=True, how='left')
