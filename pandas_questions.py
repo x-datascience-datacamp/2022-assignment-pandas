a3f3db477765b6be39014ec4344418885d05757d
Mohamed BEN JEBARA <benjeb.mohamed@gmail.com>
2023-01-13 15:21:50 +0100

    4th try

--- pandas_questions.py
+++ pandas_questions.py
@@ -29,7 +29,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     regions.rename(columns={"code": "region_code", "name": "name_reg"},
                    inplace=True)
     df = pd.merge(regions, departments,
@@ -48,7 +47,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     regions_and_departments["code_dep"].replace(
         ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
         ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
@@ -69,7 +67,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     df = referendum_and_areas[[
         'code_reg',
         'name_reg',
@@ -95,7 +92,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     gr = gpd.read_file('data/regions.geojson')
     gr.rename(columns={"code": "code_reg"}, inplace=True)
     gr.set_index(keys=["code_reg"], inplace=True)
