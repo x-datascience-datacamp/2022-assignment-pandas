9c3c9ef6c2406b8d27f6a99849eea5e6367e39b0
MARIUS-SAMO <samemarius@gmail.com>
2023-01-13 12:52:32 +0100

    fixed

--- pandas_questions.py
+++ pandas_questions.py
@@ -123,7 +123,9 @@ if __name__ == "__main__":
     referendum_and_areas = merge_referendum_and_areas(
         referendum, regions_and_departments
     )
-    referendum_results = compute_referendum_result_by_regions(referendum_and_areas)
+    referendum_results = compute_referendum_result_by_regions(
+        referendum_and_areas
+        )
     print(referendum_results)
 
     plot_referendum_map(referendum_results)
