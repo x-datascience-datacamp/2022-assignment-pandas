6ddff398809c92dc6003c2b05fe70345cb9d523e
Mohamed Vadhel Ebnou Oumar <vadel@MacBook-Pro-de-Mohamed.local>
2023-01-15 20:01:41 +0100

    first commit

--- pandas_questions.py
+++ pandas_questions.py
@@ -18,7 +18,7 @@ def load_data():
     referendum = pd.DataFrame({})
     regions = pd.DataFrame({})
     departments = pd.DataFrame({})
-
+    print("hello world")
     return referendum, regions, departments
 
 
